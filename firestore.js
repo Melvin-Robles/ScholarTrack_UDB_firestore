f96dfbd0435ca7ab2996689f4ea85d792c1fb37d
RoblesDeveloper! <eduarobles200152@gmail.com>
2024-04-28 19:45:45 -0600

    init

---
+++ firestore.js
@@ -0,0 +1,84 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAagumGjFByMFFBE4aO3qTe4Rs_Kv-xINM",
+  authDomain: "scholartrack-udb.firebaseapp.com",
+  projectId: "scholartrack-udb",
+  storageBucket: "scholartrack-udb.appspot.com",
+  messagingSenderId: "1001035970339",
+  appId: "1:1001035970339:web:dffe90b65bf752cd4e7e96",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
+
+db.collection("materias")
+  .get()
+  .then((querySnapshot) => {
+    let contenedorMaterias = document.getElementById("content-materias");
+
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      let divContent = document.createElement("div");
+      divContent.innerHTML = `   
+<div class="card">
+  <h5 class="card-header">${data.NOMBRE}</h5>
+  <div class="card-body">
+    <p class="card-text">Codigo: ${data.CODIGO}</p>
+    <p class="card-text">Codigo: ${data.ESTADO}</p>
+  </div>
+</div>
+        `;
+
+      contenedorMaterias.appendChild(divContent); 
+    });
+  });
+
+
+
+
+db.collection("grupos")
+  .get()
+  .then((querySnapshot) => {
+    let contenedorGrupos = document.getElementById("content-grupos");
+
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      let divContent = document.createElement("div");
+      divContent.innerHTML = `   
+<div class="card">
+  <h5 class="card-header">${data.NOMBRE}</h5>
+  <div class="card-body">
+    <p class="card-text">Codigo: ${data.ESTADO}</p>
+  </div>
+</div>
+        `;
+
+        contenedorGrupos.appendChild(divContent); 
+    });
+  });
+
+
+db.collection("usuarios")
+  .get()
+  .then((querySnapshot) => {
+    let contenedorUser = document.getElementById("content-user");
+
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      let divContent = document.createElement("div");
+      divContent.innerHTML = `   
+<div class="card">
+  <h5 class="card-header">${data.PRIMER_NOMBRE} ${data.PRIMER_APELLIDO} -  ${data.TIPO} </h5>
+  <div class="card-body">
+    <p class="card-text">Codigo: ${data.ESTADO}</p>
+    <p class="card-text">Codigo: ${data.EVALUACIONES}</p>
+  </div>
+</div>
+        `;
+
+        contenedorUser.appendChild(divContent); 
+    });
+  });
+
+
