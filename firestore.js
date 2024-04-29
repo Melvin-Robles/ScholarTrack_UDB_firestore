bc73f0ee89cd62e6512d631e25f1df33a5c21bc0
RoblesDeveloper! <eduarobles200152@gmail.com>
2024-04-28 20:17:01 -0600

    finish

--- firestore.js
+++ firestore.js
@@ -59,26 +59,46 @@ db.collection("grupos")
   });
 
 
-db.collection("usuarios")
+  db.collection("usuarios")
   .get()
   .then((querySnapshot) => {
     let contenedorUser = document.getElementById("content-user");
 
     querySnapshot.forEach((doc) => {
       let data = doc.data();
+
+      let evaluacionesString = Array.isArray(data.EVALUACIONES) ? data.EVALUACIONES.map((evaluacion) => {
+
+        return ` ${evaluacion.TIPO}, Puntuación: ${evaluacion.NOTA} -- ${evaluacion.MATERIA} `;
+      }).join("<br>") : "";
+
+      let materiasImpartidas = Array.isArray(data.MATERIAS_IMPARTIDAS) ? data.MATERIAS_IMPARTIDAS.map((evaluacion) => {
+
+        return `${evaluacion.MATERIA}, GRUPO: ${evaluacion.GRUPO}`;
+      }).join("<br>") : "";
+
       let divContent = document.createElement("div");
       divContent.innerHTML = `   
 <div class="card">
   <h5 class="card-header">${data.PRIMER_NOMBRE} ${data.PRIMER_APELLIDO} -  ${data.TIPO} </h5>
   <div class="card-body">
-    <p class="card-text">Codigo: ${data.ESTADO}</p>
-    <p class="card-text">Codigo: ${data.EVALUACIONES}</p>
+    <p class="card-text">Estado: ${data.ESTADO}</p>
+    ${data.CARRERA ? `
+    <p class="card-text">Carrera: ${data.CARRERA} </p>`: '' }
+
+    <p class="card-text">Fecha ingreso: ${data.FECHA_INGRESO ? data.FECHA_INGRESO.toDate().toLocaleDateString() : 'No disponible'}</p>
+    <p class="card-text">Correo: ${data.CORREO}</p>
+    <p class="card-text"> ${evaluacionesString}</p>
+    ${materiasImpartidas ? `
+    <p class="card-text">Materias impartidas: ${materiasImpartidas} </p>`: '' }
   </div>
 </div>
         `;
 
-        contenedorUser.appendChild(divContent); 
+      contenedorUser.appendChild(divContent); 
     });
-  });
+});
+
+
 
 
